399878f96eacd32be959e038c045bfe869c9925b
robert.cioltea <robertcioltea@gmail.com>
2024-01-24 15:12:47 +0200

    Create user login endpoint, handler and dto

---
+++ backEnd/SpamDetector/SpamDetector/Features/UserManagement/Login/Queries/GetUser/GetUserQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using SpamDetector.Features.UserManagement.Login.Dtos;
+using SpamDetector.Models.UserManagement;
+
+namespace SpamDetector.Features.UserManagement.Login.Queries.GetUser
+{
+    public class GetUserQuery : IRequest<string>
+    {
+        public UserLoginDto User { get; set; }
+    }
+}
