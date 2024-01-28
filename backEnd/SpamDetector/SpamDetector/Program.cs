6b69f5eaa39908787c490416b29059106f4c25d3
robert.cioltea <robertcioltea@gmail.com>
2024-01-28 20:07:34 +0200

    Remove comments and modify project structure

--- backEnd/SpamDetector/SpamDetector/Program.cs
+++ backEnd/SpamDetector/SpamDetector/Program.cs
@@ -10,10 +10,7 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
@@ -41,10 +38,10 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
             ValidateAudience = false
         };
     });
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
