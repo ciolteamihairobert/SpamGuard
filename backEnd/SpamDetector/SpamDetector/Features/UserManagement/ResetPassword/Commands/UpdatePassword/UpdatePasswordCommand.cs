3538311ec0ffa72fcbc46c63f6007e65cc5c7120
robert.cioltea <robertcioltea@gmail.com>
2024-02-12 17:03:03 +0200

    logic for reseting the password, saving the password reset token and the mail sending

---
+++ backEnd/SpamDetector/SpamDetector/Features/UserManagement/ResetPassword/Commands/UpdatePassword/UpdatePasswordCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using SpamDetector.Features.UserManagement.Register.Dtos;
+using SpamDetector.Models.UserManagement;
+
+namespace SpamDetector.Features.UserManagement.ResetPassword.Commands.UpdatePassword
+{
+    public class UpdatePasswordCommand : IRequest
+    {
+        public UserPasswordReset User { get; set; }
+    }
+}
